21fe9990b8b25e6fcfd1266cd14035f900e99350
normuminov <olmosnormuminov02@gmail.com>
2025-05-01 14:29:48 +0500

    Remove file from git cache

--- erp/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Video
-
-@receiver(post_save, sender=Video)
-def update_status_to_ready(sender, instance, created, **kwargs):
-    if created:
-        instance.status = Video.StatusChoice.READY
-        instance.save()
