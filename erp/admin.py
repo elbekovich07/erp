480e4a39ffb725df0cd6e5b1e3b5ec74f10475bf
normuminov <olmosnormuminov02@gmail.com>
2025-05-01 14:30:34 +0500

    Remove file from git cache

---
+++ erp/admin.py
@@ -0,0 +1,53 @@
+from django.contrib import admin
+from django.utils.html import format_html
+
+from erp.models import Category, Course, Student, Group, Teacher, Homework, Module, Video
+
+
+# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'student_code', 'group')
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course_id', 'teacher',)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'username', 'image_tag')
+
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-width:50px; max-height:50px"/>'.format(obj.image.url))
+        return '-'
+
+    image_tag.short_description = 'Image'
+
+
+@admin.register(Homework)
+class HomeworkAdmin(admin.ModelAdmin):
+    list_display = ('module', 'overview', 'deadline')
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'group', 'date_passed')
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'file', 'module')
---
+++ homework/files/models_PE8Hthu.py
@@ -0,0 +1,151 @@
+import random
+from datetime import timedelta
+
+from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+
+
+# Create your models here.
+
+def generate_student_code():
+    while True:
+        code = str(random.randint(10_000, 99_999))
+        if not Student.objects.filter(student_code=code).exists():
+            return code
+
+
+def default_deadline():
+    return timezone.localtime(timezone.now() + timedelta(hours=48))
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='teacher/images/', default='images/default.png')
+    username = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField(help_text="Dars davomiyligi oylarda", default=4)
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 related_name='courses')
+
+
+class Group(models.Model):
+    class StatusChoice(models.TextChoices):
+        NOT_STARTED = 'boshlanmagan'
+        ACTIVE = 'faol'
+        FINISHED = 'tugagan'
+
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey(Course,
+                               on_delete=models.CASCADE,
+                               related_name='groups')
+    teacher = models.ForeignKey(Teacher,
+                                on_delete=models.SET_NULL,
+                                related_name='groups',
+                                null=True)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField()
+    status = models.CharField(choices=StatusChoice.choices, default=StatusChoice.NOT_STARTED.value)
+
+    def __str__(self):
+        return self.name
+
+
+class Module(models.Model):
+    title = models.CharField(max_length=150)
+    group = models.ForeignKey(Group,
+                              on_delete=models.CASCADE,
+                              related_name='modules')
+    is_given = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Homework(models.Model):
+    overview = models.TextField()
+    file = models.FileField(upload_to='homework/files/')
+    deadline = models.DateTimeField(default=default_deadline,
+                                    help_text="Avtomatik: hozirgi vaqtdan 48 soat keyingi muddat"
+                                    )
+    module = models.ForeignKey(Module,
+                               on_delete=models.CASCADE,
+                               related_name='homework')
+
+    def __str__(self):
+        return self.overview
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=150)
+    video = models.FileField(upload_to='videos/')
+    created_at = models.DateTimeField(default=timezone.now)
+    module = models.ForeignKey(Module,
+                               on_delete=models.CASCADE,
+                               related_name='videos')
+
+    def __str__(self):
+        return self.title
+
+
+
+class Student(models.Model):
+    gender_choices = (
+        ("MALE", "male"),
+        ("FEMALE", "female"),
+        ("OTHER", "other"),
+    )
+
+    status = (
+        ("ACTIVE", "active"),
+        ("GRADUATED", "graduated"),
+    )
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10, choices=gender_choices, default="MALE")
+    phone_number = models.CharField(max_length=20)
+    password = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='student/images/', default='images/default.png')
+    student_code = models.CharField(
+        max_length=5,
+        unique=True,
+        default=generate_student_code,
+        editable=False,
+        help_text="Avtomatik yaratilgan 5 xonali student kod"
+    )
+    group = models.ForeignKey(Group,
+                              on_delete=models.SET_NULL,
+                              related_name='students',
+                              null=True)
+
+    def __str__(self):
+        return self.student_code
